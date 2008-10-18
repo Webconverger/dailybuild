221df2980c6cc59d2c64fa639394ec715ccfdbe5
Kai Hendry <hendry@iki.fi>
2008-10-18 15:39:37 +0200

    clean up

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -ex
-TYPE="mini"
 MIRROR="mirror.webconverger.com"
 OUTPUT="/srv/www/build.webconverger.org/output"
 TEMPDIR="$(mktemp -d -t live.XXXXXXXX)" || exit 1
-NAME=$TYPE.$1
+NAME=$1
+TYPE=$(echo $NAME | awk -F . '{print $1}')
 
 if test "$(id -u)" -ne "0"
 then
@@ -13,25 +13,27 @@ fi
 
 mailerror () {
 echo BUILD FAILED at $NAME
-echo "http://build.webconverger.com/logs/$NAME.txt" | mail -a 'From: build.webconverger.com <hendry@webconverger.com>' -s "failed" kai.hendry@gmail.com
+echo "http://build.webconverger.com/logs/$NAME.txt" |
+mail -a 'From: hendry@webconverger.com' -s "failed" kai.hendry@gmail.com
 exit 1
 }
 
-if test ! $DEBUG
+if test ! $DEBUG # Whilst debugging we might want to inspect the detritus
 then
     trap "cd $TEMPDIR/config-webc/$TYPE; lh clean --purge; rm -vrf $TEMPDIR" 0 1 2 3 9 15
 fi
 
-chmod a+rx $TEMPDIR
-cd $TEMPDIR
+chmod a+rx $TEMPDIR && cd $TEMPDIR
 
-mount # For debugging (in case /proc is mounted already)
+mount # To check in case /proc is mounted already
 
-lh --version | head -n1
-dpkg -l live-helper
+echo Live Helper Version:
+dpkg --status live-helper | egrep "^Version" | awk '{print $2}'
 
+# To check what version our sources are
 wget -q -O- http://${MIRROR}/debian/project/trace/ftp-master.debian.org
 
+# Live helper configuration (Webconverger)
 git clone git://git.debian.org/git/debian-live/config-webc.git
 
 cd config-webc/$TYPE
@@ -52,21 +54,24 @@ done
 echo "Building default (ISO)"
 time lh build || mailerror
 
-ls -lah
+ls -lah # Move build into output directory
 for f in binary.*; do mv "$f" "$OUTPUT/$NAME.${f##*.}"; done
 
-# Lets build USB now too
-sed -i 's/\(^LH_BOOTLOADER.*\)/#\1/' config/binary
-echo 'LH_BINARY_IMAGES="usb-hdd"' >> config/binary
-sed -i 's/\(^LH_SOURCE.*\)/#\1/' config/source # we've compiled sources already by default
-lh clean --binary
+if test $USB
+then
+	# Lets build USB now too
+	sed -i 's/\(^LH_BOOTLOADER.*\)/#\1/' config/binary
+	echo 'LH_BINARY_IMAGES="usb-hdd"' >> config/binary
+	sed -i 's/\(^LH_SOURCE.*\)/#\1/' config/source # we've compiled sources already by default
+	lh clean --binary
 
-echo "Building USB image"
-time lh binary || mailerror
+	echo "Building USB image"
+	time lh binary || mailerror
+	ls -lah
+	for f in binary.*; do mv "$f" "$OUTPUT/$NAME.${f##*.}"; done
+fi
 
-ls -lah
-for f in binary.*; do mv "$f" "$OUTPUT/$NAME.${f##*.}"; done
-if test -e source.tar.gz
+if test -e source.tar.gz # If LH_SOURCE is enabled
 then
 	mv source.tar.gz "$OUTPUT/$NAME.tar.gz"
 fi
--- runme.sh
+++ runme.sh
@@ -1,4 +1,4 @@
-BUILDID=$(date --rfc-3339=date)
+BUILDID="mini.$(date --rfc-3339=date)"
 LOG=/srv/www/build.webconverger.org/output/$BUILDID.txt
 if test -e $LOG
 then
@@ -6,4 +6,3 @@ then
 else
     sudo $(dirname $0)/build.sh $BUILDID  &> $LOG
 fi
-
