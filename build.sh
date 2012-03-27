0979f0b2db064afb5aff307dd1796d95c373857b
Kai Hendry <hendry@webconverger.com>
2012-03-27 06:58:54 +0200

    refined

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ if test "$DEBUG"
 then
 	echo DEBUG MODE - $TEMPDIR needs to be manually deleted
 else
-	trap "cd $TEMPDIR/Debian-Live-config/webconverger; lb clean --purge; rm -vrf $TEMPDIR" 0 1 2 3 9 15
+	trap "rm -vrf $TEMPDIR" 0 1 2 3 9 15
 fi
 
 chmod a+rx $TEMPDIR && cd $TEMPDIR
@@ -56,15 +56,7 @@ cd Debian-Live-config/webconverger
 git rev-parse HEAD
 
 # http://webconverger.org/upgrade/
-make chroot
-
-test -f binary-hybrid.iso || exit
-
-mv binary-hybrid.iso "${OUTPUT}/${NAME}.iso"
-for f in binary.*; do mv "$f" "${OUTPUT}/${NAME}.${f##*.}"; done
-echo "Redirect /latest.img /${NAME}.iso" > $OUTPUT/.htaccess
-echo "Redirect /latest.iso /${NAME}.iso" >> $OUTPUT/.htaccess
-for s in source.*; do mv "$s" "${OUTPUT}/src-${NAME}.${s##*.}"; done
+make deploy
 
 chown -R www-data:www-data $OUTPUT
 
